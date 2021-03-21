61695b2c96f2b8731e3291e0b1b60310a54d99fc
iruzts <ruizsoto1996@gmail.com>
2021-03-20 18:30:41 -0600

    Plantilla AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dash', function () {
+    return view('dash.index');
+})->name('dash');
